f892e153a8fb1078b2e29e1fe3bef21ba0cd2401
tymon <tymon.mieszkowski@gmail.com>
2024-07-08 13:29:21 +1200

    a new ASSERT_THROWS macro, to expect exceptions

--- tests/macros.cpp
+++ tests/macros.cpp
@@ -3,11 +3,16 @@
 
 #include <string>
 
+// A custom exception to throw when an assertion fails
+struct AssertionFailed : std::runtime_error {
+explicit AssertionFailed(const std::string& message) : std::runtime_error(message) {}
+};
+
 // A custom macro to assert a condition is true
 #define ASSERT( ... )                                                 \
 do {                                                                  \
     if( !( __VA_ARGS__ ) ) {                                          \
-        throw std::runtime_error(std::string("\nUnit test assert [ ") \
+        throw AssertionFailed(std::string("\nUnit test assert [ ") \
         + std::string( #__VA_ARGS__ )                                 \
         + std::string(" ] failed in line [ ")                         \
         + std::to_string(__LINE__)                                    \
@@ -16,6 +21,35 @@ do {                                                                  \
         + std::string(" ]")                                           \
         );                                                            \
     }                                                                 \
+} while( false )
+
+// A custom macro to assert that a call throws the expected exception
+#define ASSERT_THROWS( expression, exception ) \
+do { \
+    try { \
+        expression; \
+        throw AssertionFailed(std::string("\nUnit test assert_throws [ ") \
+        + std::string( #expression )                                  \
+        + std::string(" ] failed in line [ ")                         \
+        + std::to_string(__LINE__)                                    \
+        + std::string(" ] file [ ")                                   \
+        + std::string(__FILE__)                                       \
+        + std::string(" ]: No exception")                             \
+        );                                                            \
+    } catch (const exception& e) {                                    \
+        /* Expected exception */                                      \
+    } catch (const AssertionFailed& e) {                              \
+        throw e;                                                      \
+    } catch (...) {                                                   \
+        throw AssertionFailed(std::string("\nUnit test assert_throws [ ") \
+        + std::string( #expression )                                  \
+        + std::string(" ] failed in line [ ")                         \
+        + std::to_string(__LINE__)                                    \
+        + std::string(" ] file [ ")                                   \
+        + std::string(__FILE__)                                       \
+        + std::string(" ]: Unexpected exception")                     \
+        ); \
+    } \
 } while( false )
 
 #endif //DTS_91896_TEST_MACROS
---
+++ tests/test_order_list_find.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Tymon on 8/07/24.
+//
