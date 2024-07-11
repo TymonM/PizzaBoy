f5e7507a22ccd32c9cc1f3df34710793c7eb8900
tymon <tymon.mieszkowski@gmail.com>
2024-07-11 12:24:09 +1200

    documented OrderParser and MenuParser classes

--- src/menu_parser.h
+++ src/menu_parser.h
@@ -1,11 +1,29 @@
 #ifndef DTS_91896_MENU_PARSER_H
 #define DTS_91896_MENU_PARSER_H
 
-#include <nlohmann/json.hpp>
 #include "menu_item.h"
 
+#include <nlohmann/json.hpp>
+
+/**
+ * @brief The MenuParser class is responsible for converting MenuItem objects to and from a JSON format.
+ *
+ * Currently, this class provides functionality to export MenuItem objects to a JSON representation,
+ * allowing for easy serialization of menu items. Future implementations will include importing capabilities
+ * to construct MenuItem objects from JSON data.
+ */
 class MenuParser {
 public:
+    /**
+     * @brief Exports a MenuItem object to a JSON representation.
+     *
+     * This method takes a MenuItem object and converts it into a JSON object. The JSON object
+     * contains all relevant information of the MenuItem, such as its name and price, making it
+     * suitable for storage or transmission.
+     *
+     * @param item The MenuItem object to be exported.
+     * @return nlohmann::json A JSON representation of the MenuItem object.
+     */
     static nlohmann::json exportMenuItem(const MenuItem &item);
 };
 
--- src/order_parser.h
+++ src/order_parser.h
@@ -2,15 +2,52 @@
 #define DTS_91896_ORDER_PARSER_H
 
 #include "order_list.h"
-
 #include <nlohmann/json.hpp>
 
+/**
+ * @brief The OrderParser class is responsible for converting Order and OrderList objects to and from a JSON format.
+ *
+ * This class provides functionality to export Order, OrderItem, and OrderList objects to a JSON representation,
+ * facilitating the serialization of order data for storage or transmission. Future implementations will include
+ * importing capabilities to reconstruct these objects from JSON data.
+ */
 class OrderParser {
 public:
+    /**
+     * @brief Exports an OrderItem object to a JSON representation.
+     *
+     * This method takes an OrderItem object and converts it into a JSON object. The JSON object
+     * contains all relevant information of the OrderItem, such as the menu item details and quantity,
+     * making it suitable for storage or transmission.
+     *
+     * @param item The OrderItem object to be exported.
+     * @return nlohmann::json A JSON representation of the OrderItem object.
+     */
     static nlohmann::json exportOrderItem(const OrderItem &item);
+
+    /**
+     * @brief Exports an Order object to a JSON representation.
+     *
+     * This method takes an Order object and converts it into a JSON object. The JSON object
+     * includes the order's description and a list of its items, each represented as a JSON object,
+     * thereby encapsulating the complete order details in a format suitable for storage or transmission.
+     *
+     * @param order The Order object to be exported.
+     * @return nlohmann::json A JSON representation of the Order object.
+     */
     static nlohmann::json exportOrder(const Order &order);
+
+    /**
+     * @brief Exports an OrderList object to a JSON representation.
+     *
+     * This method takes an OrderList object and converts it into a JSON array. Each element of the array
+     * is a JSON object representing an individual order within the list. This facilitates the serialization
+     * of multiple orders at once, making the data suitable for storage or transmission.
+     *
+     * @param orderList The OrderList object to be exported.
+     * @return nlohmann::json A JSON representation of the OrderList object.
+     */
     static nlohmann::json exportOrderList(const OrderList &orderList);
 };
-
 
 #endif //DTS_91896_ORDER_PARSER_H
