0fd9bd0db94d145a002bc0a0a97b9d6b7b6b7293
tymon <tymon.mieszkowski@gmail.com>
2024-07-11 12:11:38 +1200

    Orders now export their descriptions as well

--- src/order_parser.cpp
+++ src/order_parser.cpp
@@ -11,6 +11,7 @@ nlohmann::json OrderParser::exportOrderItem(const OrderItem &item) {
 
 nlohmann::json OrderParser::exportOrder(const Order &order) {
     nlohmann::json j;
+    j["description"] = order.getDescription();
     j["items"] = nlohmann::json::array();
     for (const auto &item : order.getItems()) {
         j["items"].push_back(exportOrderItem(item));
--- tests/test_json.cpp
+++ tests/test_json.cpp
@@ -1,8 +1,6 @@
 #include "macros.cpp"
 
-#include "../src/menu_item.h"
 #include "../src/menu_parser.h"
-#include "../src/order_item.h"
 #include "../src/order_parser.h"
 
 void TEST_export_menu_item() {
@@ -30,6 +28,7 @@ void TEST_export_whole_order() {
     order.addItem(OrderItem(margherita, 1));
 
     auto exported = OrderParser::exportOrder(order);
+    ASSERT(exported["description"] == "For Mike, delivered to 1 Elm Ave.");
     ASSERT(exported["items"].size() == 2);
     ASSERT(exported["items"][0]["item"]["name"] == "Classic Pepperoni Pizza");
     ASSERT(exported["items"][0]["item"]["price"] == 10.0);
@@ -39,10 +38,24 @@ void TEST_export_whole_order() {
     ASSERT(exported["items"][1]["quantity"] == 1);
 }
 
+void TEST_export_order_with_special_chars() {
+    MenuItem pepperoni("Classic Pepperoni Pizza", 10.0);
+    Order order("One peppero\\ni pizza for \"Mike\",\ndelivered to \\1 Elm Ave.\"\\");
+    order.addItem(OrderItem(pepperoni, 1));
+
+    auto exported = OrderParser::exportOrder(order);
+    ASSERT(exported["description"] == "One peppero\\ni pizza for \"Mike\",\ndelivered to \\1 Elm Ave.\"\\");
+    ASSERT(exported["items"].size() == 1);
+    ASSERT(exported["items"][0]["item"]["name"] == "Classic Pepperoni Pizza");
+    ASSERT(exported["items"][0]["item"]["price"] == 10.0);
+    ASSERT(exported["items"][0]["quantity"] == 1);
+}
+
 int main() {
     TEST_export_menu_item();
     TEST_export_order_item();
     TEST_export_whole_order();
+    TEST_export_order_with_special_chars();
 
     return 0;
 }
